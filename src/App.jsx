de88821bb4827844f4ad00e603cecf4e4563f9e5
abel <abeloabate01@gmail.com>
2025-04-02 17:26:53 +0300

    a24

--- src/App.jsx
+++ src/App.jsx
@@ -213,7 +213,7 @@ function App() {
         icon: "warning",
         title: "Invalid",
         text: "Insert the image first.",
-      }); // Alert ifss the input doesn't contain a valid link
+      }); // Alert ifss the input dsoesn't contaidn a valid link
     }
   }}
 >
