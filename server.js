cb420a1685336ccd6e86d9e8a4ab9291f1af9f36
abel <abeloabate01@gmail.com>
2025-04-02 12:38:36 +0300

    a9

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import { supabase } from './src/supabaseClient.js'; // Updated import
 const app = express();
 const port = 3000;
 
-// Middleware to parse JSON
+// Middlewarve to parse JSON
 app.use(express.json());
 
 // Example route to fetch data from Supabase
